acf7d96590d142d09251dd87aeee27a570237acc
Di <helloiamdi@gmail.com>
2019-05-17 19:55:49 +1000

    Add rough raw implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
 import './App.css';
+
+const DEFAULT_VALUE = 'Enter Global ID';
+
+function Convertor() {
+  const [convertedValue, setConvertedValue] = useState(DEFAULT_VALUE);
+  const [error, setError] = useState(null);
+
+  return (
+    <Fragment>
+      <input
+        className="convertor--input"
+        autoFocus
+        onChange={(event) => {
+          const { value: globalId } = event.target;
+          let localID = DEFAULT_VALUE;
+          if (globalId) {
+            try {
+              localID = atob(globalId)
+            } catch (err) {
+              setError('Invalid global ID entered');
+            }
+          } else {
+            setError(null);
+          }
+          setConvertedValue(localID);
+        }}
+      />
+    {!error && <div className="convertor--output">{convertedValue}</div>}
+    {error && <div className="convertor--error">{error}</div>}
+    </Fragment>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page">
+      <header className="header">
+        <h1>Convert Global ID to local ID</h1>
       </header>
+      <Convertor />
+      <footer className="footer">
+        created by <a href="https://twitter.com/shetsovdm">Dmitry Shvetsov</a>
+      </footer>
     </div>
   );
 }
